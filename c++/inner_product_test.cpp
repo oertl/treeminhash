3ccf2d8af1af2f2147d81e41206b3fbf31dc192f
Otmar Ertl <otmar.ertl@dynatrace.com>
2023-01-23 16:55:45 +0100

    generalization of inner product sketching by allowing different exponents

--- c++/inner_product_test.cpp
+++ c++/inner_product_test.cpp
@@ -41,18 +41,20 @@ public:
 class InnerProductSketch {
   const vector<pair<double, double>> hashValueList;
   double norm;
+  const double exponent;
 
 public:
   InnerProductSketch(const vector<pair<double, double>> &hashValueList,
-                     double norm)
-      : hashValueList(hashValueList), norm(norm) {}
+                     double norm, double exponent)
+      : hashValueList(hashValueList), norm(norm), exponent(exponent) {}
 
   static double estimate_inner_product(const InnerProductSketch &sketch1,
                                        const InnerProductSketch &sketch2) {
 
+    assert(sketch1.exponent == sketch2.exponent);
     assert(sketch1.hashValueList.size() == sketch2.hashValueList.size());
-    const size_t m =
-        min(sketch1.hashValueList.size(), sketch2.hashValueList.size());
+    const size_t m = sketch1.hashValueList.size();
+    double exponent = sketch1.exponent;
 
     double estimatorSum = 0;
     double minHashSum = 0;
@@ -60,7 +62,8 @@ public:
       auto &[hash1, val1] = sketch1.hashValueList[j];
       auto &[hash2, val2] = sketch2.hashValueList[j];
       if (hash1 == hash2) {
-        estimatorSum += (val1 * val2) / min(val1 * val1, val2 * val2);
+        estimatorSum +=
+            (val1 * val2) / pow(min(fabs(val1), fabs(val2)), exponent);
       }
       minHashSum += min(hash1, hash2);
     }
@@ -73,35 +76,34 @@ public:
   }
 };
 
-//
 class InnerProductSketchCalculator {
   tmh::TreeMinHash<RNGFunction> treeMinHash;
+  const double exponent;
 
 public:
-  InnerProductSketchCalculator(uint32_t m, uint64_t seed)
+  InnerProductSketchCalculator(uint32_t m, uint64_t seed, double exponent = 2)
       : treeMinHash(m, RNGFunction(seed),
                     1. // set maximum supported weight equal to 1, as all
                        // weights will be <= 1. due to normalization
-        ) {}
+                    ),
+        exponent(exponent) {}
 
   InnerProductSketch compute(const vector<pair<uint64_t, double>> &input) {
 
     unordered_map<uint64_t, double> keyToValueMap(input.cbegin(), input.cend());
 
     // calculate norm
-    const double squaredNorm =
-        std::accumulate(input.cbegin(), input.cend(), 0., [](auto sum, auto x) {
+    const double norm =
+        sqrt(accumulate(input.cbegin(), input.cend(), 0., [](auto sum, auto x) {
           return sum + x.second * x.second;
-        });
-    const double norm = sqrt(squaredNorm);
+        }));
 
     // normalized and squared input for TreeMinHash
     vector<pair<uint64_t, double>> keyNormalizedSquaredValueList(input.size());
     transform(input.cbegin(), input.cend(),
               keyNormalizedSquaredValueList.begin(), [&](auto &keyValue) {
                 return make_pair(keyValue.first,
-                                 (keyValue.second * keyValue.second) /
-                                     squaredNorm);
+                                 pow(fabs(keyValue.second) / norm, exponent));
               });
 
     // weighted minhash
@@ -116,78 +118,89 @@ public:
                                  keyToValueMap[keyHash.first] / norm);
               });
 
-    return InnerProductSketch(hashValueList, norm);
+    return InnerProductSketch(hashValueList, norm, exponent);
   }
 };
 
 int main(int argc, char *argv[]) {
 
-  uint64_t numCycles = 100;
+  uint64_t numCycles = 1000;
   uint64_t sizeOfWeightList = 1000;
   uint32_t m = 1 << 14;
 
-  uint64_t seedForRandomGenerator = UINT64_C(0x0c2954b1cb065f32);
+  // test different exponents
+  vector<double> exponentValues = {0, 0.5, 1, 1.5, 2, 3};
+
+  uint64_t seedForWeights = UINT64_C(0x0c2954b1cb065f32);
+  uint64_t seedForKeys = UINT64_C(0x11da3e19c9262418);
   uint64_t seedForTreeMinHash = UINT64_C(0xda47270740231451);
 
-  mt19937_64 rng(seedForRandomGenerator);
-
+  // generate some weight vectors
+  mt19937_64 rngForWeights(seedForWeights);
   uniform_real_distribution<double> nonZeroValueDistribution1(-2.3, 1);
   uniform_real_distribution<double> nonZeroValueDistribution2(-3, 10);
   bernoulli_distribution zeroDistribution1(0.1);
   bernoulli_distribution zeroDistribution2(0.2);
-
-  // generate some weight vectors
   double trueInnerProduct = 0.;
   vector<pair<double, double>> weights(sizeOfWeightList);
   for (uint64_t i = 0; i < sizeOfWeightList; ++i) {
-    double weight1 =
-        zeroDistribution1(rng) ? 0. : nonZeroValueDistribution1(rng);
-    double weight2 =
-        zeroDistribution2(rng) ? 0. : nonZeroValueDistribution2(rng);
+    double weight1 = zeroDistribution1(rngForWeights)
+                         ? 0.
+                         : nonZeroValueDistribution1(rngForWeights);
+    double weight2 = zeroDistribution2(rngForWeights)
+                         ? 0.
+                         : nonZeroValueDistribution2(rngForWeights);
     weights[i] = {weight1, weight2};
     trueInnerProduct += weight1 * weight2;
   }
 
-  double sumError = 0;
-  double sumSquaredError = 0;
+  for (double exponent : exponentValues) {
 
-  InnerProductSketchCalculator sketch_calculator(m, seedForTreeMinHash);
+    mt19937_64 rngForKeys(seedForKeys);
 
-  for (uint64_t j = 0; j < numCycles; ++j) {
+    double sumError = 0;
+    double sumSquaredError = 0;
 
-    // create random input by associating random keys with given weights
-    vector<pair<uint64_t, double>> input1;
-    vector<pair<uint64_t, double>> input2;
-    for (uint64_t i = 0; i < weights.size(); ++i) {
-      uint64_t val = rng();
-      double weight1 = weights[i].first;
-      double weight2 = weights[i].second;
-      if (weight1 != 0.)
-        input1.emplace_back(val, weight1);
-      if (weight2 != 0.)
-        input2.emplace_back(val, weight2);
+    InnerProductSketchCalculator sketch_calculator(m, seedForTreeMinHash,
+                                                   exponent);
+    for (uint64_t j = 0; j < numCycles; ++j) {
+
+      // create random input by associating random keys with given weights
+      vector<pair<uint64_t, double>> input1;
+      vector<pair<uint64_t, double>> input2;
+      for (uint64_t i = 0; i < weights.size(); ++i) {
+        uint64_t key = rngForKeys();
+        double weight1 = weights[i].first;
+        double weight2 = weights[i].second;
+        if (weight1 != 0.)
+          input1.emplace_back(key, weight1);
+        if (weight2 != 0.)
+          input2.emplace_back(key, weight2);
+      }
+
+      // compute sketches for inner product
+      auto sketch1 = sketch_calculator.compute(input1);
+      auto sketch2 = sketch_calculator.compute(input2);
+
+      // estimate inner product
+      double estimatedInnerProduct =
+          InnerProductSketch::estimate_inner_product(sketch1, sketch2);
+
+      double error = estimatedInnerProduct - trueInnerProduct;
+      sumError += error;
+      sumSquaredError += error * error;
     }
+    const double relativeEstimationBias =
+        sumError / fabs(trueInnerProduct) / numCycles;
+    const double relativeEstimationRmse =
+        sqrt(sumSquaredError) / fabs(trueInnerProduct) / numCycles;
 
-    // compute sketches for inner product
-    InnerProductSketch sketch1 = sketch_calculator.compute(input1);
-    InnerProductSketch sketch2 = sketch_calculator.compute(input2);
-
-    // estimate inner product
-    double estimatedInnerProduct =
-        InnerProductSketch::estimate_inner_product(sketch1, sketch2);
-
-    double error = estimatedInnerProduct - trueInnerProduct;
-    sumError += error;
-    sumSquaredError += error * error;
+    cout << "exponent = " << exponent << endl;
+    cout << "true inner product = " << trueInnerProduct << endl;
+    cout << "relative estimation bias = " << relativeEstimationBias << endl;
+    cout << "relative estimation rmse = " << relativeEstimationRmse << endl;
+    cout << endl;
   }
-  const double relativeEstimationBias =
-      sumError / fabs(trueInnerProduct) / numCycles;
-  const double relativeEstimationRmse =
-      sqrt(sumSquaredError) / fabs(trueInnerProduct) / numCycles;
-
-  cout << "true inner product = " << trueInnerProduct << endl;
-  cout << "relative estimation bias = " << relativeEstimationBias << endl;
-  cout << "relative estimation rmse = " << relativeEstimationRmse << endl;
 
   return 0;
 }
